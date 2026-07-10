5464c6633e2fc98d49e9cec3e30724c9f11f0d5b
happyness980817 <happyness980817@gmail.com>
2026-07-10 17:39:58 +0900

    5.4 LLMs and Agents

--- content-pipeline-agent/main.py
+++ content-pipeline-agent/main.py
@@ -1,7 +1,33 @@
-from typing import cast
+from typing import List, cast
 
 from crewai.flow.flow import Flow, listen, start, router, and_, or_
+from crewai import Agent
+from crewai import LLM
 from pydantic import BaseModel
+from tools import web_search_tool
+
+
+class BlogPost(BaseModel):
+    title: str
+    subtitle: str
+    sections: List[str]
+
+
+class Tweet(BaseModel):
+    content: str
+    hashtags: str
+
+
+class LinkedInPost(BaseModel):
+    hook: str
+    content: str
+    call_to_action: str
+
+
+class Score(BaseModel):
+
+    score: int = 0
+    reason: str = ""
 
 
 class ContentPipelineState(BaseModel):
@@ -12,10 +38,11 @@ class ContentPipelineState(BaseModel):
 
     # Internal
     max_length: int = 0
-    score: int = 0
+    research: str = ""
+    score: Score | None = None
 
     # Content
-    blog_post: str = ""
+    blog_post: BlogPost | None = None
     tweet: str = ""
     linkedin_post: str = ""
 
@@ -31,7 +58,9 @@ class ContentPipelineFlow(Flow[ContentPipelineState]):
         if self.typed_state.content_type not in ["tweet", "blog", "linkedin"]:
             raise ValueError("Wrong content type.")
 
-        if self.topic == "":  # Just to validate that we passed a topic as an input.
+        if (
+            self.typed_state.topic == ""
+        ):  # Just to validate that we passed a topic as an input.
             raise ValueError("The topic can't be blank")
 
         if self.typed_state.content_type == "tweet":
@@ -43,8 +72,18 @@ class ContentPipelineFlow(Flow[ContentPipelineState]):
 
     @listen(init_content_pipeline)
     def conduct_research(self):
-        print("Researching...")
-        return True
+
+        researcher = Agent(
+            role="Head Researcher",
+            backstory="You're like a digital detective who loves digging up fascinating facts and insights. You have a knack for finding the good stuff that others miss.",
+            goal=f"Find the most interesting and useful info about {self.typed_state.topic}",
+            tools=[web_search_tool],
+        )
+
+        research = researcher.kickoff(
+            f"Find the most interesting and useful info about {self.typed_state.topic}"
+        )
+        self.typed_state.research = str(research)
 
     @router(conduct_research)
     def conduct_research_router(self):
@@ -59,35 +98,74 @@ class ContentPipelineFlow(Flow[ContentPipelineState]):
 
     @listen(or_("make_blog_post", "remake_blog_post"))
     def handle_make_blog(self):
-        # If a blog post has been make, show the old one to the AI and ask it to improve the post.
+        # If a blog post has been made, show the old one to the AI and ask it to improve the post.
         # Else, just ask it to create one.
-        print("Writing a blog post...")
+
+        blog_post = self.typed_state.blog_post
+
+        llm = LLM(model="openai/o4-mini", response_format=BlogPost)
+
+        if blog_post is None:
+            self.typed_state.blog_post = llm.call(f"""
+            Make a blog post on the topic {self.typed_state.topic} using the following research:
+
+            <research>
+            ================
+            {self.typed_state.research}
+            ================
+            </research>
+            """)
+
+        else:
+            self.typed_state.blog_post = llm.call(f"""
+            You wrote this blog post on {self.typed_state.topic}, but it does not have a good SEO score because of {self.typed_state.score.reason if self.typed_state.score else "it needs improvement"} 
+            
+            Improve it.
+
+            <blog post>
+            {self.typed_state.blog_post.model_dump_json()}
+            </blog post>
+
+            Use the following research.
+
+            <research>
+            ================
+            {self.typed_state.research}
+            ================
+            </research>
+            """)
 
     @listen(or_("make_tweet", "remake_tweet"))
     def handle_make_tweet(self):
-        # If a tweet has been make, show the old one to the AI and ask it to improve the tweet.
+        # If a tweet has been made, show the old one to the AI and ask it to improve the tweet.
         # Else, just ask it to create one.
         print("Writing a tweet post...")
 
     @listen(or_("make_linkedin_post", "remake_linkedin_post"))
     def handle_make_linkedin(self):
-        # If a linkedin post has been make, show the old one to the AI and ask it to improve the post.
+        # If a linkedin post has been made, show the old one to the AI and ask it to improve the post.
         # Else, just ask it to create one.
         print("Writing a linkedin post...")
 
     @listen(handle_make_blog)
     def check_seo(self):
+        print(self.typed_state.blog_post)
+        print(self.typed_state.blog_post)
+        print("============")
+        print(self.typed_state.research)
         print("Checking Blog SEO...")
+        self.typed_state.score = Score(score=8, reason="SEO check passed.")
 
     @listen(or_(handle_make_tweet, handle_make_linkedin))
     def check_virality(self):
         print("Checking Virality...")
+        self.typed_state.score = Score(score=8, reason="Virality check passed.")
 
     @router(or_(check_seo, check_virality))
     def score_router(self):
 
         content_type = self.typed_state.content_type
-        score = self.typed_state.score
+        score = self.typed_state.score.score if self.typed_state.score else 0
 
         if score >= 8:
             return "check_passed"
@@ -108,9 +186,9 @@ flow = ContentPipelineFlow()
 
 flow.plot()
 
-# flow.kickoff(
-#     inputs={
-#         "content_type": "tweet",
-#         "topic": "AI Dog Training",
-#     },
-# )
+flow.kickoff(
+    inputs={
+        "content_type": "tweet",
+        "topic": "AI Dog Training",
+    },
+)
