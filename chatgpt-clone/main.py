e9d18c2b96688079f064c4ebc337688999285033
happyness980817 <happyness980817@gmail.com>
2026-07-27 14:26:46 +0900

    7.8 Welcome To Streamlit

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from chatgpt-clone!")
+import streamlit as st
+import time
 
+st.header("Hello World!")
 
-if __name__ == "__main__":
-    main()
+st.button("Click me please!")
+
+st.text_input(
+    "Write your API KEY",
+    max_chars=100,
+)
+
+st.feedback("faces")
+
+with st.sidebar:
+    st.badge("Badge 1")
+
+tab1, tab2, tab3 = st.tabs(["Agent", "Chat", "Output"])
+
+with tab1:
+    st.header("Agent")
+with tab2:
+    st.header("Chat")
+with tab3:
+    st.header("Output")
+
+with st.chat_message("ai"):
+    st.text("Hello!")
+    with st.status("Agent is using tools...") as status:
+        time.sleep(1)
+        status.update(label="Agent is searching the web...")
+        time.sleep(2)
+        status.update(label="Agent is reading the pages...")
+        time.sleep(3)
+        status.update(label="Task Done.")
+        status.update(state="complete")
+
+st.chat_input("Write a message for the assistant.")
